133e1d6b14450de7bb02166c944e578d564e361f
appaKappaK <ballsmj3@pm.me>
2026-05-25 00:53:46 -0400

    Cleans stale OnGameSettingsApplied subscribers before Start() and removes them on OnDestroy prefix/postfix by both owner and delegate

--- src/NoMoreLeaks/Patches/VesselAutopilotUILeakPatch.cs
+++ src/NoMoreLeaks/Patches/VesselAutopilotUILeakPatch.cs
@@ -2,12 +2,34 @@ using HarmonyLib;
 
 namespace NoMoreLeaks.Patches
 {
-    [HarmonyPatch(typeof(VesselAutopilotUI), "OnDestroy")]
+    [HarmonyPatch(typeof(VesselAutopilotUI))]
     internal static class VesselAutopilotUILeakPatch
     {
-        private static void Postfix(VesselAutopilotUI __instance)
+        [HarmonyPrefix]
+        [HarmonyPatch("Start")]
+        private static void StartPrefix()
         {
-            EventCleanup.RemoveGameEvent(GameEvents.OnGameSettingsApplied, __instance, "onGameParametersChanged");
+            EventCleanup.RemoveDestroyedOwners(GameEvents.OnGameSettingsApplied, typeof(VesselAutopilotUI));
+        }
+
+        [HarmonyPrefix]
+        [HarmonyPatch("OnDestroy")]
+        private static void OnDestroyPrefix(VesselAutopilotUI __instance)
+        {
+            Cleanup(__instance);
+        }
+
+        [HarmonyPostfix]
+        [HarmonyPatch("OnDestroy")]
+        private static void OnDestroyPostfix(VesselAutopilotUI __instance)
+        {
+            Cleanup(__instance);
+        }
+
+        private static void Cleanup(VesselAutopilotUI instance)
+        {
+            EventCleanup.RemoveOwner(GameEvents.OnGameSettingsApplied, instance);
+            EventCleanup.RemoveGameEvent(GameEvents.OnGameSettingsApplied, instance, "onGameParametersChanged");
         }
     }
 }
